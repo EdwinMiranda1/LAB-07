df22380424d4d380ad19181d775f832926cdd5c7
EdwinMiranda1 <darkalister23@gmail.com>
2024-04-14 10:28:58 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import { List } from './components/List'
+import { WapperList } from './components/WrapperList'
+// import { Button }  from './components/Button'
+
+function App() {
+
+  return (
+    <>
+      
+      <WapperList>
+      <ul>
+          <li> Pan </li>
+          <li> Queso </li>
+          <li> Mantequilla </li>
+      </ul>  
+      </WapperList>  
+            
+    </>
+  )
+}
+
+export default App
